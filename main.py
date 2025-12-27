2780211a38b0adf673c8e34c7a5d5cca32b9e27d
mayabarlev10-hub <maya.barlev10@gmail.com>
2025-12-27 14:26:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,18 @@ X = df[selected_features]
 y = df['status']
 
 from sklearn.preprocessing import MinMaxScaler
-
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 
 from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 from sklearn.neighbors import KNeighborsClassifier
-
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train, y_train)
 
 
 from sklearn.metrics import accuracy_score
-
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
